a0f69b3d219d69d7eb6c94a3d072128937a0a1da
WILMER IMEMDOZA <wmendozar.cief.pow@gmail.com>
2025-05-07 12:23:07 +0200

    Esto es un comentario de Actualizacion

--- queryPrestamosMayoresUnMesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const empleadosController = require('../controllers/prestamosController');
-
-// Consulta Prestamo libros morosos +1mes
-router.get('/query', prestamosController.getPrestamosLibrosMoraMasdeUnMes);
-
-module.exports = router;
